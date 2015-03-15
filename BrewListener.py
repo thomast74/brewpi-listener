092e6bb86547bab78120236692d23f38906c5187
Thomas Trageser <thomast74@gmail.com>
2015-03-15 22:01:55 +0000

    Inital commit of application daemon

---
+++ BrewListener.py
@@ -0,0 +1,75 @@
+import ConfigParser
+import logging
+import os
+import RequestHandler
+import SocketServer
+import sys
+import threading
+
+
+from daemon import runner
+
+
+class ThreadedUDPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
+    pass
+
+
+class BrewListenerApp():
+
+    logger = logging.getLogger("BrewListenerApp")
+    ip_address = "192.168.2.255"
+    port = 7872
+
+    def __init__(self, ip_address, port, pid_file, stdout, stderr):
+        self.ip_address = ip_address
+        self.port = port
+        self.stdin_path = '/dev/null'
+        self.stdout_path = stdout
+        self.stderr_path = stderr
+        self.pidfile_path = pid_file
+        self.pidfile_timeout = 5
+
+    def run(self):
+        # Get configuration from CFG file
+
+        # prepare UDP server
+        self.logger.info("Listen to {}:{}".format(self.ip_address, self.port))
+        server = ThreadedUDPServer((self.ip_address, self.port), RequestHandler.BrewListenerRequestHandler)
+
+        # start UDP server
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.daemon = True
+
+        while True:
+            if not server_thread.is_alive():
+                server_thread.start()
+
+
+def main(config_file):
+    config = ConfigParser.RawConfigParser()
+    config.read(config_file)
+
+    # ser variables
+    RequestHandler.POST_URL = config.get('server', 'post-url')
+
+    # setup logging
+    log_level = config.get('server', 'log-level')
+    numeric_level = getattr(logging, log_level.upper(), None)
+
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % log_level)
+
+    logging.basicConfig(filename=config.get("server", "log-file"), filemode="a+", level=numeric_level,
+                        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    # start daemon
+    logging.info("Starting up application")
+    app = BrewListenerApp(config.get("server", "ip-address"), config.getint("server", "port"),
+                          config.get("server", "pid-file"), config.get("server", "out-log-file"),
+                          config.get("server", "err-log-file"))
+    daemon_runner = runner.DaemonRunner(app)
+    daemon_runner.daemon_context.files_preserve = [logging.root.handlers[0].stream]
+    daemon_runner.do_action()
+
+if __name__ == "__main__":
+    main(sys.argv[2])
---
+++ RequestHandler.py
@@ -0,0 +1,51 @@
+import json
+import logging
+import requests
+import SocketServer
+import sys
+
+
+# global variables
+POST_URL = ""
+
+
+class BrewListenerRequestHandler(SocketServer.BaseRequestHandler):
+
+    logger = logging.getLogger("BrewListenerRequestHandler")
+
+    # UDP request coming in
+    def handle(self):
+        try:
+            json_string = self.request[0].strip()
+            socket = self.request[1]
+
+            self.logger.info("Handle message from {}".format(self.client_address[0]))
+
+            if self.validate_json(json_string):
+                self.logger.debug("Received Status from {}".format(self.client_address[0]))
+                socket.sendto("OK", self.client_address)
+                self.send_status_to_api_server(json_string)
+            else:
+                self.logger.error("Received Invalid Json Status")
+                socket.sendto("ERROR", self.client_address)
+        except:
+            e = sys.exc_info()[0]
+            self.logger.error(e)
+
+    def validate_json(self, json_string):
+        try:
+            jsonData = json.loads(json_string)
+            return True
+        except ValueError:
+            return False
+
+    def send_status_to_api_server(self, status):
+        try:
+            # open connection and post json to http://localhost/breqpi/api/status
+            self.logger.debug("POST to Url {}".format(POST_URL))
+
+            response = requests.post(POST_URL, data=json.dumps(status))
+            self.logger.debug("RESPONSE: {} {}".format(response.status_code, response.text))
+        except:
+            e = sys.exc_info()[0]
+            self.logger.error(e)
