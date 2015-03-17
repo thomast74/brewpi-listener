e7a7b9856670e012f285592c1547bdf45f7d0de9
Thomas Trageser <thomast74@gmail.com>
2015-03-17 20:59:24 +0000

    Test case re-write

--- BrewPiListenerTest.py
+++ BrewPiListenerTest.py
@@ -25,27 +25,32 @@ import socket
 import sys
 
 
-config = ConfigParser.RawConfigParser()
-config.read("BrewPiListener.cfg")
+def main():
+    config = ConfigParser.RawConfigParser()
+    config.read("BrewPiListener.cfg")
 
-PORT = config.getint('server', 'port')
-data = "{" \
-       "  \"version\": \"0.1\", " \
-       "  \"revision\": \"C\", " \
-       "  \"device-id\": \"xx-xx-xx-xx-xx\", " \
-       "  \"ip-address\": \"0.1\", " \
-       "  \"status\": \"ACTIVE\" " \
-       "}"
+    PORT = config.getint('server', 'port')
+    data = "{" \
+           "  \"version\": \"0.1\", " \
+           "  \"revision\": \"C\", " \
+           "  \"device-id\": \"xx-xx-xx-xx-xx\", " \
+           "  \"ip-address\": \"0.1\", " \
+           "  \"status\": \"ACTIVE\" " \
+           "}"
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-print "\nSending data to 192.168.2.255"
-sock.sendto(data + "\n", ("192.168.2.255", PORT))
+    print "\nSending data to 192.168.2.255"
+    sock.sendto(data + "\n", ("192.168.2.255", PORT))
 
-print "Sent:     {}".format(data)
-ready = select.select([sock], [], [], 1)
-if ready[0]:
-    data = sock.recv(512)
-    print "Received: {}".format(data)
+    print "Sent:     {}".format(data)
+    ready = select.select([sock], [], [], 1)
+    if ready[0]:
+        data = sock.recv(512)
+        print "Received: {}".format(data)
+
+
+if __name__ == "__main__":
+    main()
